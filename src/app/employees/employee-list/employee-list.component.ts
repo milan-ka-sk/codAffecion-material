c0ad77e3ffdf1a5376e0015cf5181b844bcd1d34
Milan Koniar <milankoniar@gmail.com>
2019-07-09 22:35:41 +0200

    added basic pagination

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { EmployeeService } from 'src/app/shared/employee.service';
 import {MatTableDataSource} from '@angular/material/table';
-import { MatSort } from '@angular/material';
+import { MatSort, MatPaginator } from '@angular/material';
 
 @Component({
   selector: 'app-employee-list',
@@ -13,6 +13,7 @@ export class EmployeeListComponent implements OnInit {
   listData: MatTableDataSource<any>;
   displayedColumns: string[] = ['fullName', 'email', 'mobile', 'city', 'actions'];
   @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   constructor(private service: EmployeeService) { }
 
@@ -28,6 +29,7 @@ export class EmployeeListComponent implements OnInit {
 
         this.listData = new MatTableDataSource(array);
         this.listData.sort = this.sort;
+        this.listData.paginator = this.paginator;
       }
     );
   }
